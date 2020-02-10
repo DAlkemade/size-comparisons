d7799bb1b68c212882ddf41a9224d3214982ddb4
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2020-02-10 15:27:27 +0000

    Reformat file

--- retrieve_frequencies.py
+++ retrieve_frequencies.py
@@ -1,10 +1,10 @@
 import io
-import numpy as np
+
 
 class FrequencyRetriever:
 
     def __init__(self):
-        self.unigrams = {} # TODO: maybe change to list and use np.searchsorted to find unigram
+        self.unigrams = {}  # TODO: maybe change to list and use np.searchsorted to find unigram
         with io.open('data/frequencies/1gms/vocab', 'r', encoding='utf8') as f:
             text = f.read()
             lines = text.split('\n')
@@ -17,17 +17,14 @@ class FrequencyRetriever:
     def retrieve_unigram_frequency(self, unigram):
         return self.unigrams[unigram]
 
-
-
     def retrieve_frequency(self, ngram):
         n = len(ngram.split())
         if n == 1:
             return self.retrieve_unigram_frequency(ngram)
-        #TODO
+        # TODO
         return None
 
 
 if __name__ == '__main__':
     retriever = FrequencyRetriever()
     print(retriever.retrieve_frequency('apple'))
-
