d93c604798c733698a90f7cb1b587cf7d0c02c1b
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2020-03-05 15:33:56 +0000

    Improve tests

--- tests/test_retrieve_html.py
+++ tests/test_retrieve_html.py
@@ -5,28 +5,30 @@ from google import retrieve_htmls, create_or_update_urls_html
 
 
 def test_retrieve_htmls():
+    """Check whether the html retrieval method works."""
     results = {'test': ['html']}
     urls = {'tiger123': ['https://en.wikipedia.org/wiki/Tiger', 'https://seaworld.org/animals/all-about/tiger/characteristics/']}
-    retrieve_htmls(results, ['tiger123'], urls)
+    labels = ['tiger123']
+    retrieve_htmls(results, labels, urls)
     assert 'test' in results.keys()
-    assert 'tiger123' in results.keys()
-    assert type(results['tiger123']) is list
-    assert len(results['tiger123']) > 0
+    for label in labels:
+        assert label in results.keys()
+        assert type(results[label]) is list
+        assert len(results[label]) > 0
 
 
 def test_loading_updating_saving():
+    """Test whether the file creation, updating, saving, etc of the html retrieval works."""
     fname = 'test_htmls.p'
     file_path = os.path.join('../tmp', fname)
     try:
         os.remove(file_path)
     except FileNotFoundError:
         pass
-    label1 = 'tiger123'
-    label2 = 'helicopter123'
-    labels = [label1, label2]
-    urls = {label1: ['https://en.wikipedia.org/wiki/Tiger',
+    labels = ['tiger123', 'helicopter123']
+    urls = {labels[0]: ['https://en.wikipedia.org/wiki/Tiger',
                          'https://seaworld.org/animals/all-about/tiger/characteristics/'],
-            'helicopter123': ['https://www.popularmechanics.com/flight/a2150/4224761/']}
+            labels[1]: ['https://www.popularmechanics.com/flight/a2150/4224761/']}
 
 
     create_or_update_urls_html(file_path, labels, urls)
@@ -34,8 +36,8 @@ def test_loading_updating_saving():
     results: dict = pickle.load(open(file_path, 'rb'))
     assert type(results) is dict
     assert len(results.keys()) == 2
-    assert type(results[label1]) is list
     for label in labels:
+        assert type(results[label]) is list
         assert len(results[label]) == len(urls[label])
         for res in results[label]:
             assert res.status_code == 200
