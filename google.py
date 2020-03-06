10be34335ad55329cf9bdaf1ccafef2608d540d4
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2020-03-06 14:26:41 +0000

    Doesn\t seem to work

--- google.py
+++ google.py
@@ -3,6 +3,8 @@ import pickle
 import requests
 import tqdm
 from googlesearch import search
+import asyncio
+import aiohttp
 
 
 def retrieve_query(query: str) -> list:
@@ -39,27 +41,26 @@ def create_or_update_results(file_path: str, queries: list, keys: list):
     pickle.dump(results, open(file_path, 'wb'))
 
 
-def retrieve_htmls(results, labels, urls: dict):
-    for i in tqdm.trange(len(labels)):
-        label = labels[i]
-        if label not in results.keys():
-            htmls = []
+async def retrieve_htmls_for_object(label, results, urls):
+    print("Start for new label")
+    if label not in results.keys():
+        htmls = []
+        try:
+            urls_for_name = urls[label]
+        except KeyError:
+            print(f"URLs not available for label {label}")
+            urls_for_name = None
+        for url in urls_for_name:
             try:
-                urls_for_name = urls[label]
-            except KeyError:
-                print(f"URLs not available for label {label}")
-                urls_for_name = None
-            for url in urls_for_name:
-                try:
-                    r = requests.get(url)
-                    if r.status_code == 200:
-                        htmls.append(r.text)
-                    else:
-                        print(f"No statuscode 200 for url {url}")
-                except Exception as e:
-                    print(f'Couldnt find url {url}, {e}')
+                r = requests.get(url)
+                if r.status_code == 200:
+                    htmls.append(r.text)
+                else:
+                    print(f"No statuscode 200 for url {url}")
+            except Exception as e:
+                print(f'Couldnt find url {url}, {e}')
 
-            results[label] = htmls
+        results[label] = htmls
 
 
 def create_or_update_urls_html(file_path: str, keys: list, urls: dict):
@@ -72,9 +73,32 @@ def create_or_update_urls_html(file_path: str, keys: list, urls: dict):
         results = dict()
 
     try:
-        retrieve_htmls(results, keys, urls)
+        gather_htmls(results, keys, urls)
     except Exception as e:  # TODO specify error (403 http)
         print(e)
         print("Something went wrong, saving intermediate result")
 
     pickle.dump(results, open(file_path, 'wb'))
+
+
+async def request(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            return await resp.text()
+
+
+async def main(results: dict, labels: list, urls: dict):
+    results = await asyncio.gather(
+        *[retrieve_htmls_for_object(label, results, urls) for label in labels]
+    )
+    print(len(results))
+    print(results)
+
+
+def gather_htmls(results, keys, urls: dict):
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main(results, keys, urls))
+        loop.run_until_complete(loop.shutdown_asyncgens())
+    finally:
+        loop.close()
