13eadc800adcef5347b0e3a2983325134350996a
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2020-03-16 16:43:42 +0100

    Add extra samples and use ttest to compare

--- size_comparisons/inference/baseline_numeric_gaussians.py
+++ size_comparisons/inference/baseline_numeric_gaussians.py
@@ -5,7 +5,8 @@ from size_comparisons.parse_objects import InputsParser
 import pandas as pd
 import numpy as np
 from scipy import stats
-
+# scipy.stats.f_oneway
+# scipy.stats.ttest_ind
 
 class BaselineNumericGaussians(object):
 
@@ -13,12 +14,20 @@ class BaselineNumericGaussians(object):
         self.data = data
 
     def retrieve_mu_std(self, name: str) -> (float, float):
+        row = self.retrieve_row(name)
+        return row['mean'].values[0], row['std'].values[0]
+
+    def retrieve_row(self, name):
         row = self.data.loc[self.data['name'] == name]
         if row.empty:
             raise RuntimeWarning(f"Unknown object {name}")
-        return row['mean'].values[0], row['std'].values[0]
+        return row
 
-    def larger_than(self, object1: str, object2: str) -> float:
+    def retrieve_sizes(self, name):
+        row = self.retrieve_row(name)
+        return row['sizes'].values[0]
+
+    def larger_than_simple(self, object1: str, object2: str) -> float:
         mu1, std1 = self.retrieve_mu_std(object1)
         mu2, std2 = self.retrieve_mu_std(object2)
         if math.isnan(mu1) or math.isnan(mu2):
@@ -28,6 +37,17 @@ class BaselineNumericGaussians(object):
         std = np.sqrt(std1**2 + std2**2)
         cdf0 = stats.norm(mu_combine, std).cdf(0.)
         return 1 - cdf0
+
+    def ttest(self, object1: str, object2: str):
+        # TODO how to use the fact that we know sizes can't be negative
+        sizes1 = self.retrieve_sizes(object1)
+        sizes2 = self.retrieve_sizes(object2)
+        tvalue, p = stats.ttest_ind(sizes1, sizes2, equal_var=False)
+        # p value: assuming the null hypothesis (the population means are the same) is true, what is the probability
+        # of seeing the data that we are seeing or more extreme
+        mean_larger = self.larger_than_simple(object1, object2) > .5
+
+        return p, mean_larger
 
 
 def main():
@@ -45,11 +65,13 @@ def main():
             print("Couldn't find one of the objects, skipping this pair")
             continue
         try:
-            p_1_bigger_than_2 = baseline.larger_than(name1, name2)
+            ttest, mean_larger = baseline.ttest(name1, name2)
         except RuntimeWarning:
             print(f"Unreliable result for {name1}, {name2}, not showing")
             continue
-        print(f'{name1} is bigger than {name2} with p={p_1_bigger_than_2}')
+        print(f'Mean of {name1} is {"" if mean_larger else "not "}bigger than {name2}, null hypothesis p_value is '
+              f'{ttest} and the difference is thus '
+              f'{"" if ttest <= 0.05 else "not"} significant')
 
 
 if __name__ == "__main__":
