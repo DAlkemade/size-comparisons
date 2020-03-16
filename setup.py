94f11b76086ab36deef8520c18e1d7676f49dc51
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2020-03-16 15:21:07 +0100

    Add package setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="size_comparisons", # Replace with your own username
+    version="0.0.1",
+    author="Daan Alkemade",
+    author_email="author@example.com",
+    description="Package for thesis Daan Alkemade",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.7',
+)
