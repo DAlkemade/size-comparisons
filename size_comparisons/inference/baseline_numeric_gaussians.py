9527731e452250521033775ac3b57875249a3e17
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2020-03-25 12:26:27 +0100

    Add cache to seed graph

--- size_comparisons/inference/baseline_numeric_gaussians.py
+++ size_comparisons/inference/baseline_numeric_gaussians.py
@@ -2,11 +2,9 @@ import math
 
 import numpy as np
 import pandas as pd
+import tqdm
 from scipy import stats
 
-
-# scipy.stats.f_oneway
-# scipy.stats.ttest_ind
 
 class BaselineNumericGaussians(object):
     # TODO add ttest results cache for prepopulation
@@ -18,7 +16,18 @@ class BaselineNumericGaussians(object):
         """
         self.data = data
         data_size = self.data.shape[0]
-        self.pvalue_cache = np.full((data_size, data_size), np.nan)
+        self.adjancency_matrix_cache = np.full((data_size, data_size), np.nan)
+
+    def fill_adjacency_matrix(self):
+        for i in tqdm.tqdm(self.data.index):
+            for j in self.data.index:
+                if i != j:
+                    # TODO handle singlevalue and empty lists
+                    sizes1 = self.data.iloc[i]['sizes']
+                    sizes2 = self.data.iloc[j]['sizes']
+                    tvalue, p = stats.ttest_ind(sizes1, sizes2, equal_var=False)
+                    self.adjancency_matrix_cache[i, j] = tvalue
+                    self.adjancency_matrix_cache[j, i] = -tvalue
 
     def retrieve_mu_std(self, name: str) -> (float, float):
         row = self.retrieve_row(name)
@@ -34,22 +43,29 @@ class BaselineNumericGaussians(object):
         row = self.retrieve_row(name)
         return row['sizes'].values[0]
 
-    def ttest(self, object1: str, object2: str):
+    def ttest(self, object1: str, object2: str) -> float:
         # TODO how to use the fact that we know sizes can't be negative
         # TODO think about using the ztest (maybe increase number of pages scraped), because having gaussian might be
         # more useful
-        sizes1 = self.retrieve_sizes(object1)
-        sizes2 = self.retrieve_sizes(object2)
-        tvalue, p = stats.ttest_ind(sizes1, sizes2, equal_var=False)  # welch ttest (unequal variances)
-        # https://blog.minitab.com/blog/adventures-in-statistics-2/understanding-t-tests-1-sample-2-sample-and-paired-t-tests
-        # p value: assuming the null hypothesis (the population means are the same) is true, what is the probability
-        # of seeing the data that we are seeing or more extreme
+        index1 = self.data.index[self.data['name'] == object1][0]
+        index2 = self.data.index[self.data['name'] == object2][0]
+        cached_value = self.adjancency_matrix_cache[index1, index2]
+        if math.isnan(cached_value):
+            sizes1 = self.retrieve_sizes(object1)
+            sizes2 = self.retrieve_sizes(object2)
+            tvalue, p = stats.ttest_ind(sizes1, sizes2, equal_var=False)  # welch ttest (unequal variances)
+            # https://blog.minitab.com/blog/adventures-in-statistics-2/understanding-t-tests-1-sample-2-sample-and-paired-t-tests
+            # p value: assuming the null hypothesis (the population means are the same) is true, what is the probability
+            # of seeing the data that we are seeing or more extreme
+        else:
+            tvalue = cached_value
 
-        return p, tvalue > 0
+        return tvalue
 
 
 def find_confidences_for_pairs_lazy(data: pd.DataFrame, test_pairs_tuples: list):
     baseline = BaselineNumericGaussians(data)
+    # baseline.fill_adjacency_matrix()
     for pair in test_pairs_tuples:
         try:
             name1 = pair.object1
@@ -58,10 +74,10 @@ def find_confidences_for_pairs_lazy(data: pd.DataFrame, test_pairs_tuples: list)
             print("Couldn't find one of the objects, skipping this pair")
             continue
         try:
-            ttest, mean_larger = baseline.ttest(name1, name2)
+            t_statistic = baseline.ttest(name1, name2)
         except RuntimeWarning:
             print(f"Unreliable result for {name1}, {name2}, not showing")
             continue
-        print(f'Mean of {name1} is {"" if mean_larger else "not "}bigger than {name2}, null hypothesis p_value is '
-              f'{ttest} and the difference is thus '
-              f'{"" if ttest <= 0.05 else "not"} significant')
+        print(
+            f'Mean of {name1} is {"" if t_statistic > 0 else "not "}bigger than {name2}, null hypothesis tstatistic is '
+            f'{t_statistic}')
