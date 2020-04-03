3e6ab15fadf907e77590c6e0d49b874b6c995909
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2020-04-03 15:07:36 +0200

    Update error_analysis_regex.py

--- examples/exploration/error_analysis_regex.py
+++ examples/exploration/error_analysis_regex.py
@@ -9,7 +9,7 @@ pp = pprint.PrettyPrinter()
 PRINT_HTML = False
 
 
-analyzed_terms = ['n11860555']
+analyzed_terms = ['n01581984']
 
 def main():
     input_parser = InputsParser()
@@ -17,13 +17,15 @@ def main():
         htmls = input_parser.retrieve_google_results_html()
     synset_names = [retrieve_synset(label)._name for label in analyzed_terms]
     regex_sizes = input_parser.retrieve_regex_scraper_sizes()
-    # regex_contexts = input_parser.retrieve_regex_scraper_contexts()
+    regex_contexts = input_parser.retrieve_regex_scraper_contexts()
     for i, term in enumerate(analyzed_terms):
         pp.pprint(term)
         pp.pprint(synset_names[i])
         if PRINT_HTML:
             with open('htmls.txt', 'w') as f:
                 f.writelines(htmls[term])
+        else:
+            pp.pprint(regex_contexts[term])
         pp.pprint(regex_sizes[term])
         # pp.pprint(regex_contexts[term])
 
