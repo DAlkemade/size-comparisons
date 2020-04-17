9ba61f5630b8ea218488a0d8a5f821a5900dd8fb
Daan <28051445+DAlkemade@users.noreply.github.com>
2020-04-17 16:42:54 +0200

    Add counts to infobox analysis

--- size_comparisons/exploration/explore_infoboxes.py
+++ size_comparisons/exploration/explore_infoboxes.py
@@ -7,6 +7,9 @@ from bs4 import BeautifulSoup
 from requests import get
 from size_comparisons.parse_objects import InputsParser
 from size_comparisons.scraping.analyze import retrieve_synset
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Record:
@@ -17,6 +20,7 @@ class Record:
         self.height = None
         self.size = None
         self.top_level_synset = None
+        self.count = None
 
 
 def check_contains_height_length(url):
@@ -39,7 +43,7 @@ names = inputparser.retrieve_names()
 labels = inputparser.retrieve_labels()
 records = [Record(name, labels[i]) for i, name in enumerate(names)]
 random.seed(42)
-records = random.sample(records, 5)
+records = random.sample(records, 30)
 del names
 del labels
 
@@ -57,16 +61,40 @@ for record in records:
     synset = retrieve_synset(record.label)
     record.category = synset.lexname()
 
+ngram_count_lookup = inputparser.retrieve_frequencies()
+for record in records:
+    count = int(ngram_count_lookup[record.name])
+    record.count = count
+
+
 data_dict = dict()
 data_dict['names'] = [record.name for record in records]
 data_dict['height'] = [record.height for record in records]
 data_dict['size'] = [record.size for record in records]
 data_dict['length'] = [record.length for record in records]
 data_dict['category'] = [record.category for record in records]
+data_dict['count'] = [record.count for record in records]
 
 df = pd.DataFrame(data=data_dict)
-groups = df.groupby(['category']).mean()
+df['any'] = (df['height']) | (df['size']) | (df['length'])
+groups = df.groupby(['category']).agg(['mean', 'size'])
 print(groups)
 groups.to_csv(inputparser.data_dir / 'infoboxes.csv')
 
+print(df.groupby(['category']).size())
+
 print(f'total means: {df.mean()}')
+
+x = df['count'].values
+anys = df['any'].values
+print(x)
+print(anys)
+
+
+bin_means, bin_edges, binnumber = stats.binned_statistic(x, anys, 'sum', bins=10)
+plt.figure()
+plt.plot(x, anys, 'b.', label='raw data')
+plt.hlines(bin_means, bin_edges[:-1], bin_edges[1:], colors='g', lw=5,
+           label='binned statistic of data')
+plt.legend()
+plt.show()
