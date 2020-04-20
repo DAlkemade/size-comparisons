f1f315132d86105bffea3c8b5400af4926f3a1cd
Daan <28051445+DAlkemade@users.noreply.github.com>
2020-04-20 15:25:35 +0200

    Update html_scraper.py

--- size_comparisons/scraping/html_scraper.py
+++ size_comparisons/scraping/html_scraper.py
@@ -7,7 +7,7 @@ import tqdm
 from typing import Dict
 from bs4 import BeautifulSoup
 
-CONCURRENT_TASKS = 40
+CONCURRENT_TASKS = 50
 ObjectURL = namedtuple('ObjectURL', ['url', 'index', 'label', 'position_in_order'])
 
 
